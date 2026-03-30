1f98f379e8f73ce9e099eb12876598d0eadb44f6
luisda696 <luisda696@gmail.com>
2026-03-29 20:11:34 -0400

    Create constants.js

---
+++ js/utils/constants.js
@@ -0,0 +1,8 @@
+export const MIN_TIEBREAK_DIFF = 2;
+export const DEFAULT_TIEBREAK_POINTS = 7;
+export const MAX_GAMES_PER_SET = 15;
+export const POINT_SYSTEM = {
+  WIN: 3,
+  DRAW: 1,
+  LOSS: 0
+};
